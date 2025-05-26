5497f25342292de69745c0a498d1648cfb298a90
Kevin <kevinsehnem123@gmail.com>
2025-05-26 17:18:53 -0300

    Finished the landingpage, adding the middleware Clerk for authentication

---
+++ components/logo.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: [
+        "100",
+        "200",
+        "300",
+        "400", 
+        "500", 
+        "600", 
+        "700"
+    ],
+});
+
+export const Logo = () => {
+    return (
+        <Link href="/">
+            <div className="hover:opacity-75 transition items-center gap-x-2 
+            hidden md:flex">
+                <Image 
+                src="/logo.png" 
+                alt="logo"
+                height={40}
+                width={40}
+                />
+                <p className={cn("text-lg text-neutral-700 pb-1",
+                poppins.className,)}>PostUp</p>
+            </div>
+
+        </Link>
+    )
+}
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "PostUp",
+    description: "A platform to collaborate, improve the productivity and task managment.",
+};
