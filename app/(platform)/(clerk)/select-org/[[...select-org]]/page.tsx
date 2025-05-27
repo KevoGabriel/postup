454acf615a674c9623448c0887d5f485f0f9a95c
Kevin <kevinsehnem123@gmail.com>
2025-05-27 17:13:07 -0300

    Added a new page, a protected one that cames after the auth

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,13 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganizationPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+    <OrganizationList 
+    hidePersonal 
+    afterSelectOrganizationUrl="/organization/:id"
+    afterCreateOrganizationUrl="/organization/:id"
+    />
+    </div>
+  );
+}
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+const OrganizationIdPage = () => {
+    return (
+        <div>
+            <h1>Organization Page</h1>
+            <p>This is the page for a specific organization.</p>
+        </div>
+    )
+}
+
+export default OrganizationIdPage;
