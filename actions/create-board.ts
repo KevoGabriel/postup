1372ae729b0f91fdb8d369c5268687bb1a6ac765
Kevin <kevinsehnem123@gmail.com>
2025-06-02 17:16:15 -0300

    setting up schema

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,13 +1,24 @@
 "use server";
 
+import { z } from "zod";
+
 import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+const CreateBoard = z.object({
+  title: z.string(),
+});
 
 export async function create(formData: FormData) {
-  const title = formData.get("title") as string;
+  const { title } = CreateBoard.parse({
+    title: formData.get("title"),
+  });
 
   await db.board.create({
     data: {
       title,
     },
   });
+
+  revalidatePath("/organization/org_2xplTnD5EpVnJyWYl6rLKakVKgL");
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,12 @@
 import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
+    <div className="flex flex-col space-y-4">
       <form action={create}>
         <input
           id="title"
@@ -11,7 +15,15 @@ const OrganizationIdPage = () => {
           placeholder="Enter a board title"
           className="border-black border p-1"
         />
+        <Button type="submit" className="ml-2">
+          Submit
+        </Button>
       </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board title: {board.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
